26a62db3c4281aaed95c897e246de44ec548ce9e
Unknown <shriram.varadarajan@gmail.com>
2018-04-14 20:35:44 -0500

    Changed some variables

--- chap1.10.c
+++ chap1.10.c
@@ -1,11 +1,12 @@
+/*
 #include <stdio.h>
-#define MAXLINE 1000 /* maximum input line size */
-int max; /* maximum length seen so far */
-char line[MAXLINE]; /* current input line */
-char longest[MAXLINE]; /* longest line saved here */
+#define MAXLINE 1000  maximum input line size
+int max;  maximum length seen so far
+char line[MAXLINE];  current input line
+char longest[MAXLINE];  longest line saved here
 int getline(void);
 void copy(void);
-/* print longest input line; specialized version */
+ print longest input line; specialized version
 main()
 {
 	int len;
@@ -17,12 +18,12 @@ main()
 		max = len;
 		copy();
 	}
-	if (max > 0) /* there was a line */
+	if (max > 0)  there was a line
 		printf("%s", longest);
 	return 0;
 }
 
-/* getline: specialized version */
+ getline: specialized version
 int getline(void)
 {
 	int c, i;
@@ -36,7 +37,7 @@ int getline(void)
 	line[i] = '\0';
 	return i;
 }
-/* copy: specialized version */
+ copy: specialized version
 void copy(void)
 {
 	int i;
@@ -46,7 +47,8 @@ void copy(void)
 		++i;
 }
 
-/*
+
 In certain circumstances, the extern declaration can be omitted. If the definition of the external variable occurs in the source file before its use in a particular function, then there is no need for an extern declaration in the function. The extern declarations in main, getline and copy are thus redundant. In fact, common practice is to place definitions of all external variables at the beginning of the source file, and then omit all extern declarations.
 If the program is in several source files, and a variable is defined in file1 and used in file2 and file3, then extern declarations are needed in file2 and file3 to connect the occurrences of the variable. The usual practice is to collect extern declarations of variables and functions in a separate file, historically called a header, that is included by #include at the front of each source file. The suffix .h is conventional for header names. The functions of the standard library, for example, are declared in headers like <stdio.h>.
+
 */
