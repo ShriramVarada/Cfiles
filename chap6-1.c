c2b8218ccb896701c7f51eeb44c8906b8dca7f1d
Unknown <shriram.varadarajan@gmail.com>
2018-05-19 14:52:36 -0500

    Added chapter 6

---
+++ chap6-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "chap6-1.h"
+
+Point makePoint(int x, int y){
+	Point temp={x,y};
+	return temp;
+}
+Point addpoint(Point p1, Point p2){
+	p1.x += p2.x;
+	p1.y += p2.y;
+	return p1;
+}
+int ptinRect(Point p1, Rectangle r1){
+	return p1.x >= r1.pt1.x && p1.x < r1.pt2.x && p1.y >= r1.pt1.y && p1.y < r1.pt2.y;
+}
+
+Rectangle canonrect(struct rect r)
+{
+	struct rect temp;
+	temp.pt1.x = min(r.pt1.x, r.pt2.x);
+	temp.pt1.y = min(r.pt1.y, r.pt2.y);
+	temp.pt2.x = max(r.pt1.x, r.pt2.x);
+	temp.pt2.y = max(r.pt1.y, r.pt2.y);
+	return temp;
+}
+
+int main(void){
+	Point a = { 0, 0};
+	Point b = {10, 10};
+	Rectangle rect = {a,b};
+	Rectangle* r = &rect;
+	printf("%d %d", r->pt1.x, r->pt2.x);
+	Point middle = makePoint(((double)a.x+b.x)/2, ((double)a.y+b.y)/2);
+	return EXIT_SUCCESS;
+}
---
+++ chap6-1.h
@@ -0,0 +1,27 @@
+/*
+ * chap6-1.h
+ *
+ *  Created on: May 19, 2018
+ *      Author: Shriram
+ */
+
+#ifndef CHAP6_1_H_
+#define CHAP6_1_H_
+
+#define min(a, b) ((a>b) ? b : a)
+#define max(a, b) ((a>b) ? a : b)
+
+struct point{ int x; int y;};
+typedef struct point Point;
+struct rect { Point pt1; Point pt2;};
+typedef struct rect Rectangle;
+
+extern Point makePoint(int, int);
+
+extern Point addpoint(Point, Point);
+
+extern int ptinRect(Point, Rectangle);
+
+Rectangle canonrect(Rectangle r);
+
+#endif /* CHAP6_1_H_ */
